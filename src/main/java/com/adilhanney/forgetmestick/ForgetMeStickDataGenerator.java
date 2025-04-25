93bb8e868166c127a602e32f3b3e3e86773d9acd
Adil Hanney <adilhanney@disroot.org>
2025-04-25 22:55:25 +0100

    Initial commit

---
+++ src/main/java/com/adilhanney/forgetmestick/ForgetMeStickDataGenerator.java
@@ -0,0 +1,11 @@
+package com.adilhanney.forgetmestick;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ForgetMeStickDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
